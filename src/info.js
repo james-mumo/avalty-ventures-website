3bea3e5c3bd7989f84ffe2e51801bb4502de5293
james-mumo <mumojames98@gmail.com>
2023-12-27 00:36:49 +0300

    first commit

---
+++ src/info.js
@@ -0,0 +1,29 @@
+const companyInfo = {
+  companyName: "Example Company",
+  founderCEO: "Otieno Otieno",
+  location: "Nairobi, Kenya",
+  services: [
+    "Web Development",
+    "Digital Marketing",
+    "Consulting",
+    "Add Some More If Any",
+  ],
+  testimonials: [
+    {
+      name: "Otieno Otieno",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      imageUrl: "https://example.com/testimonial1.jpg",
+    },
+    {
+      name: "Otieno Otieno",
+      message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      imageUrl: "https://example.com/testimonial1.jpg",
+    },
+  ],
+  email: "info@example.com",
+  phoneNumber: "+1 (555) 123-4567",
+  // Add any other information you want to include that seem relevant
+  // ...
+};
+
+module.exports = companyInfo;
