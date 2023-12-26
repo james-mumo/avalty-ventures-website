548a2b566bc4776b361f565083d358fb0b4ba160
james-mumo <mumojames98@gmail.com>
2023-12-27 01:10:31 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import './index.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // All pages
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import DemoProduct from './pages/DemoProduct';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import DemoProduct from "./pages/DemoProduct";
 
-import {useDocTitle} from './components/CustomHook';
-import ScrollToTop from './components/ScrollToTop';
+import { useDocTitle } from "./components/CustomHook";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   useEffect(() => {
     const aos_init = () => {
       AOS.init({
         once: true,
-        duration: 1000,
-        easing: 'ease-out-cubic',
+        duration: 700,
+        easing: "ease-out-cubic",
       });
-    }
+    };
 
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       aos_init();
     });
   }, []);
 
-  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+  useDocTitle("Avalty Ventures || Innovate, Transform, Excel with Us");
 
   return (
     <>
@@ -39,13 +35,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/get-demo" element={<DemoProduct />} /> 
+            <Route path="/get-demo" element={<DemoProduct />} />
           </Routes>
         </ScrollToTop>
       </Router>
     </>
   );
 }
-
 
 export default App;
