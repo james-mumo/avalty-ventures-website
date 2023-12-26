908fa6f409bb9a49c4ea72665873e17771432ebc
james-mumo <mumojames98@gmail.com>
2023-12-27 02:33:24 +0300

    new

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,46 +1,68 @@
-import React from 'react';
-import kws from '../images/clients/kws.png';
-import geps from '../images/clients/geps.png';
-import protergia from '../images/clients/protergia.png';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import prestige from "../images/clients/prestige.png";
+import cl1 from "../images/clients/cl1.png";
+import cred from "../images/clients/cred.png";
+import gad from "../images/clients/gad.png";
 
-const clientImage = {
-    height: '10rem',
-    width: 'auto',
-    mixBlendMode: 'colorBurn'
-}
+const clientData = [
+  { name: "PrestigeLink", title: "Inventory Tracking System", image: prestige },
+  { name: "CreditFit", title: "Company Website", image: cred },
+  { name: "DigiKey", title: "EMS System", image: cl1 },
+  { name: "DG Gadget Store", title: "Gadget E-Commerce Store", image: gad },
+];
 
 const Clients = () => {
-    return (
-        <div className="mt-8 bg-gray-100">
-            <section data-aos="fade-up">
-                <div className="my-4 py-4">
-                        <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Our Clients</h2>
-                        <div className='flex justify-center'>
-                            <div className='w-24 border-b-4 border-blue-900'></div>
-                        </div>
-                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">Some of our clients.</h2>
-                    </div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Show 3 slides at a time (adjust as needed)
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, // 3 seconds
+  };
 
-                <div className="p-16" data-aos="fade-in" data-aos-delay="600">
-                    <div className="grid sm:grid-cols-3 lg:grid-cols-3">
-                        <div style={clientImage} className= "overflow-hidden flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100 w-1/6">
-                            <img src={kws} alt="client" />                           
-                        </div>
-
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={protergia} alt="client" />                            
-                        </div> 
-
-                        <div style={clientImage} className="overflow-hidden p-3 flex justify-center transition-all ease-in-out opacity-50 hover:opacity-100">
-                            <img src={geps} alt="client" />                            
-                        </div>
-
-                                           
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="mt-8 bg-gray-100">
+      <section data-aos="fade-up">
+        <div className="my-0 py-4">
+          <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">
+            Our Clients
+          </h2>
+          <div className="flex justify-center">
+            <div className="w-24 border-b-2 border-blue-900"></div>
+          </div>
+          <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">
+            Some of our clients.
+          </h2>
         </div>
-    )
-}
+
+        <div className="p-10" data-aos="fade-in" data-aos-delay="600">
+          <Slider {...settings}>
+            {clientData.map((client, index) => (
+              <div
+                key={index}
+                className="overflow-hidden flex justify-center w-[100px] h-[300px]"
+              >
+                <img
+                  src={client.image}
+                  alt={client.name}
+                  className="w-[270px] h-[255px] mx-auto rounded-md border bg-slate-500"
+                />
+                <div className="text-center">
+                  <p className="text-lg font-semibold">{client.name}</p>
+                  <p className="text-sm">{client.title}</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Clients;
