f2139ad80da8ea1cbfcee8c22a5c235f43ddcb5e
wildcard98 <mumojames98@gmail.com>
2024-01-17 15:05:30 +0300

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -37,23 +37,23 @@ const Services = () => {
               </div>
             </div>
 
-            // <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
-            //   <div className="m-2 text-justify text-sm">
-            //     <img
-            //       alt="card img"
-            //       className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out"
-            //       src={img2}
-            //     />
-            //     <h2 className="font-semibold my-4 text-2xl text-center">
-            //       Mobile App Development
-            //     </h2>
-            //     <p className="text-md font-medium">
-            //       We develop high-quality, custom cross-platform mobile
-            //       applications that are robust and optimized for performance,
-            //       scalability, and accessibility.
-            //     </p>
-            //   </div>
-            // </div>
+            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+              <div className="m-2 text-justify text-sm">
+                <img
+                  alt="card img"
+                  className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out"
+                  src={img2}
+                />
+                <h2 className="font-semibold my-4 text-2xl text-center">
+                  Mobile App Development
+                </h2>
+                <p className="text-md font-medium">
+                  We develop high-quality, custom cross-platform mobile
+                  applications that are robust and optimized for performance,
+                  scalability, and accessibility.
+                </p>
+              </div>
+            </div>
 
             <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
               <div className="m-2 text-justify text-sm">
